44ec60032a3f6bd279cff4a9e9a7f587eb06875f
MSantoso52 <santosomulyo52@gmail.com>
2025-12-11 05:44:23 +0700

    Update SQL code & input data

--- customerrevenue.sql
+++ customerrevenue.sql
@@ -2,17 +2,17 @@
 -- 1. customer table view
 CREATE OR REPLACE TABLE customers AS
 SELECT *
-FROM read_parquet('/home/mulyo/Learning/duckdb/parquet/customer_data.parquet');
+FROM read_parquet('/home/mulyo/Learning/duckdb/data/customer_data.parquet');
 
 -- 2. order table view
 CREATE OR REPLACE TABLE orders AS
 SELECT *
-FROM read_parquet('/home/mulyo/Learning/duckdb/parquet/order_data.parquet');
+FROM read_csv('/home/mulyo/Learning/duckdb/data/order_data.csv');
 
 -- 3. order_item table view
 CREATE OR REPLACE TABLE order_item AS
 SELECT *
-FROM read_parquet('/home/mulyo/Learning/duckdb/parquet/order_item_data.parquet');
+FROM read_json('/home/mulyo/Learning/duckdb/data/order_item_data.json');
 
 -- 4 insight ready: customer revenue
 WITH customerrevenue AS(
